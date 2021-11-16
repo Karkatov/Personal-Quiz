a63eeb1781a934c06a56be2aff2988e804150a34
Duxxless <Karkatof@ya.ru>
2021-11-16 20:45:35 +0300

    Updated application structure

---
+++ Personal Quiz/Model/Question.swift
@@ -0,0 +1,12 @@
+//
+//  Question.swift
+//  Personal Quiz
+//
+//  Created by Duxxless on 16.11.2021.
+//
+
+struct Question {
+    let text: String
+    let type: ResponseType
+    let answers: [String]
+}
---
+++ Personal Quiz/Model/ResponseType.swift
@@ -0,0 +1,13 @@
+//
+//  ResponseType.swift
+//  Personal Quiz
+//
+//  Created by Duxxless on 16.11.2021.
+//
+
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
